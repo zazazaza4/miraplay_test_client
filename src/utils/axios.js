52209b6c8655ed85243520955c62fd45bad71a0b
Yaroslav <6715414@gmail.com>
2023-11-15 02:31:59 +0200

    feat: add config for axios

---
+++ src/utils/axios.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    if (!config?.headers) {
+      throw new Error(
+        `Expected 'config' and 'config.headers' not to be undefined`
+      );
+    }
+    config.headers.Authorization = window.localStorage.getItem('token') || '';
+
+    return config;
+  },
+  (error) => error
+);
+
+export default instance;
