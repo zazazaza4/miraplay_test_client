0c0ebd0c80bd48d0b11cd7f78d3a7400db98bc6f
Yaroslav <6715414@gmail.com>
2023-11-15 14:58:49 +0200

    refactor: add handleError and logout

--- src/hooks/useGamesQuery.js
+++ src/hooks/useGamesQuery.js
@@ -1,29 +1,52 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 
 import { getGames } from '@/api/games';
+import { logout } from '@/store/slices/authSlice';
+import { HOME_ROUTE } from '@/constants';
 
 export const useGames = (page, isFreshFirst = true, genre, gamesToShow) => {
   const [games, setGames] = useState([]);
   const [isOver, setIsOver] = useState(false);
   const [gamesListLength, setGamesListLength] = useState(gamesToShow);
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleError = (error) => {
+    if (error.response && error.response.status === 401) {
+      dispatch(logout());
+      localStorage.clear();
+
+      navigate(HOME_ROUTE);
+    } else {
+      console.error('An error occurred:', error.message);
+    }
+  };
+
   const { isLoading, refetch } = useQuery({
     queryKey: ['get games'],
     queryFn: async () => {
-      const data = await getGames(page, isFreshFirst, genre, gamesToShow);
+      try {
+        const data = await getGames(page, isFreshFirst, genre, gamesToShow);
 
-      setGames((prev) => [...prev, ...data.games]);
-      setGamesListLength(data.gamesListLength);
+        setGames((prev) => [...prev, ...data.games]);
+        setGamesListLength(data.gamesListLength);
 
-      if (data.gamesListLength === gamesListLength) {
-        setIsOver(true);
-      } else {
-        setIsOver(false);
+        if (data.gamesListLength === gamesListLength) {
+          setIsOver(true);
+        } else {
+          setIsOver(false);
+        }
+
+        return data;
+      } catch (error) {
+        handleError(error);
       }
-
-      return data;
     },
+    onError: handleError,
   });
 
   return {
