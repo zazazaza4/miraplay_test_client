95d965f578443c4c0f50cca2ebb78207a806a98e
Yaroslav <6715414@gmail.com>
2023-11-15 11:13:13 +0200

    feat: implement useGames hook for fetching and managing game data

---
+++ src/hooks/useGamesQuery.js
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+import { getGames } from '@/api/games';
+
+export const useGames = (page, isFreshFirst = true, genre, gamesToShow) => {
+  const [games, setGames] = useState([]);
+  const [isOver, setIsOver] = useState(false);
+  const [gamesListLength, setGamesListLength] = useState(gamesToShow);
+
+  const { isLoading, refetch } = useQuery({
+    queryKey: ['get games'],
+    queryFn: () => async () => {
+      const data = await getGames(page, isFreshFirst, genre, gamesToShow);
+
+      setGames(data.games);
+      setGamesListLength(data.gamesListLength);
+
+      if (data.gamesListLength === gamesListLength) {
+        setIsOver(true);
+      } else {
+        setIsOver(false);
+      }
+
+      return data;
+    },
+  });
+
+  return {
+    isLoading,
+    games,
+    refetch,
+    isOver,
+    length: gamesListLength,
+  };
+};
