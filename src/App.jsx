224d8b03ee3adf13fc50291c822bcaf146df69fb
Yaroslav <6715414@gmail.com>
2023-11-15 11:21:01 +0200

    style: add ToastContainer to the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 import AppRouter from '@/routes/AppRouter';
 import { Header } from '@/components/header';
 import { Footer } from '@/components/footer';
 import { ScrollUpButton } from '@/components/scroll-to-button';
 
+import 'react-toastify/dist/ReactToastify.css';
 import '@/assets/scss/styles.scss';
 
 const queryClient = new QueryClient({
@@ -32,7 +34,10 @@ function App() {
         </main>
         <Footer />
       </Router>
+
+      <ToastContainer />
       <ScrollUpButton />
+
       {fetchDevTools}
     </QueryClientProvider>
   );
