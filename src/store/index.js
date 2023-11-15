09636ad41b49fe04d10ce5a9391103788c3ebbdc
Yaroslav <6715414@gmail.com>
2023-11-15 11:19:04 +0200

    refactor: remove notificationReducer from store configuration

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from './slices/authSlice';
 import authFormReducer from './slices/authFormSlice';
-import notificationReducer from './slices/notificationSlice';
 import { axiosMiddleware } from './middlewares/axiosMiddleware';
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     authForm: authFormReducer,
-    notification: notificationReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(axiosMiddleware),
