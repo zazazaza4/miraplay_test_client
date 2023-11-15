af038c745b813962e203e3a55ed0d7aa98aa3379
Yaroslav <6715414@gmail.com>
2023-11-15 18:32:12 +0200

    feat: add PropTypes to GameCard component

--- src/components/game-card/index.jsx
+++ src/components/game-card/index.jsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion';
+import PropTypes from 'prop-types';
 
 import { Image } from '@/components/image';
 import { truncateString } from '@/utils/truncate';
@@ -11,9 +12,10 @@ const GameCard = ({
   gameImage,
   inTop,
   genre,
+  onClick,
 }) => {
   return (
-    <motion.li className={styles.card}>
+    <motion.li className={styles.card} onClick={onClick}>
       <Image src={gameImage} className={styles.img} alt="game" loading="lazy" />
       <div className={styles.info}>
         <h4 className={styles.title}>{commonGameName}</h4>
@@ -28,6 +30,22 @@ const GameCard = ({
       </div>
     </motion.li>
   );
+};
+
+GameCard.propTypes = {
+  commonGameName: PropTypes.string.isRequired,
+  gameDescription: PropTypes.string,
+  gameImage: PropTypes.string.isRequired,
+  inTop: PropTypes.bool,
+  genre: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+GameCard.defaultProps = {
+  inTop: false,
+  genre: '',
+  gameDescription: '',
+  onClick: null,
 };
 
 export { GameCard };
